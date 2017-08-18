96c5bbc6405e6ef3b4455ca7963b945366f5ed23
nkdxczh <zhc416@lehigh.edu>
2017-08-18 17:46:12 -0400

    add changing alpha

--- varyingtest3/webgl-demo.js
+++ varyingtest3/webgl-demo.js
@@ -146,12 +146,12 @@ function initBuffers() {
   // for each face.
 
   var colors = [
-    [1.0,  1.0,  1.0,  1.0],    // Front face: white
-    [1.0,  0.0,  0.0,  1.0],    // Back face: red
-    [0.0,  1.0,  0.0,  1.0],    // Top face: green
-    [0.0,  0.0,  1.0,  1.0],    // Bottom face: blue
+    [1.0,  1.0,  1.0,  0.7],    // Front face: white
+    [1.0,  0.0,  0.0,  0.3],    // Back face: red
+    [0.0,  1.0,  0.0,  0.5],    // Top face: green
+    [0.0,  0.0,  1.0,  0.0],    // Bottom face: blue
     [1.0,  1.0,  0.0,  1.0],    // Right face: yellow
-    [1.0,  0.0,  1.0,  1.0]     // Left face: purple
+    [1.0,  0.0,  1.0,  0.88]     // Left face: purple
   ];
 
   // Convert the array of colors into a table for all the vertices.
